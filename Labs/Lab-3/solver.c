36a8533427c59804d4c6aa96c6e5f8dd2be468ac
Rhys Agombar <rhys.agombar@uoit.net>
2015-10-30 13:40:17 -0400

    Unfucking the naming conventions

--- Labs/Lab-3/solver.c
+++ Labs/Lab-3/solver.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
-#include "utility.h"
 
-int** Garr;
+int Garr[9][9];
 
-void load(int* arr[9][9]){
+void load(int arr[9][9]){
   FILE *f;
   f = fopen("puzzle.txt","r");
 
@@ -26,25 +25,25 @@ int* allocate_mem(int*** arr, int n, int m){
   return arr_data;
 }
 
-void deallocate_mem(int*** arr, int* arr_data){
+void deallocate_mem(int*** arr, int* arr_data){ // Borked
   free(arr_data);
   free(*arr);
 }
 
 int main (int argc, char *argv[]){
 
-  allocate_mem(&Garr,9,9);
+  int* arr_data = allocate_mem(&Garr,9,9);
 
   load(&Garr);
 
   for (int y = 0; y < 9; y++){
     for (int x = 0; x < 9; x++){
-      printf("before");
       printf("%d ",Garr[x][y]);
-      printf("after");
     }
     printf("\n");
   }
+
+  //deallocate_mem(&Garr,&arr_data); borked.
 
   return 0;
 }
