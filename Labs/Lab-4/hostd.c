8d55b77a5e3e03e5374acf1f77fb14f6ea960e72
Rhys Agombar <RhysAgombar@users.noreply.github.com>
2015-11-24 01:49:49 -0500

    Editing from web because git does not like my apartment's internet.

--- Labs/Lab-4/hostd.c
+++ Labs/Lab-4/hostd.c
@@ -19,16 +19,77 @@
 
 // Put macros or constants here using #define
 #define MEMORY 1024
+#define MAX_LEN 256
+int* proc_inf[10][8];
 
 // Put global environment variables here
 
 // Define functions declared in hostd.h here
+void load_file(int* rq[10][8])
+{
+    //Open file and ready for reading
+    FILE *f;
+    f = fopen("dispatchlist","r");
+    char line[MAX_LEN];
+
+    //Read in the resource requirements
+    for (int i = 0; i < 10; i++){
+        fgets(line,MAX_LEN,f);
+        // Temporary solution for until after I get the rest of the stuff working
+        sscanf(line, "%d, %d, %d, %d, %d, %d, %d, %d", &rq[i][0],&rq[i][1],&rq[i][2],&rq[i][3],&rq[i][4],&rq[i][5],&rq[i][6],&rq[i][7],&rq[i][8]);
+    }
+}
+
+proc create_proc(int i){
+    proc ret = new proc();
+    //unfinished
+}
+
 
 int main(int argc, char *argv[])
 {
+    int printers = 2;
+    int scanner = 1;
+    int modem = 1;
+    int cd = 2;
+    int mem = MEMORY;
+
+    load_file(proc_inf);
+
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 9; j++){
+            printf("%d ", proc_inf[i][j]);
+        }
+        printf("\n");
+    }
+
+    queue rt;
+    queue p1, p2, p3;
+
+    for(int time = 0; time < 10; time++){
+        for (int i = 0; i < 10; i++){
+            
+            if (proc_inf[i][0] == time){
+
+                proc item = create_proc(i);
+
+                if(proc_inf[i][1] == 0){
+                    push(rt,item);
+                } else if (proc_inf[i][1] == 1){
+                    push(p1,item);
+                } else if (proc_inf[i][1] == 2) {
+                    push(p2,item);
+                } else if (proc_inf[i][1] == 3){
+                    push(p3,item);
+                }
+
+
+            }
+        }
+    }
+
+
     // ==================== YOUR CODE HERE ==================== //
-    
-    // Load the dispatchlist
     
     // Add each process structure instance to the job dispatch list queue
 
@@ -43,5 +104,5 @@ int main(int argc, char *argv[])
 
     // Repeat until all processes have been executed, all queues are empty
      
-    return EXIT_SUCCESS;
+    return 0;
 }
